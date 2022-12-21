e0d7cff699817bf10f611282f6bbbb3bc7c74016
David E. Franco G <david.elsensei3@gmail.com>
2022-12-21 14:39:55 -0400

    Add files via upload

--- day21/part2.py
+++ day21/part2.py
@@ -5,7 +5,7 @@ from aoc_utils import test_input, get_raw_data, process_data
 from aoc_utils import DelayedOP, OPERATION
 import operator
 import poly # a library I make for simple polinomials calculations
-
+import math
 
 
 def main(data:str, show=False) -> int:
@@ -41,19 +41,49 @@ def main(data:str, show=False) -> int:
         print("x=",namespace[root.x])
         print("y=",namespace[root.y])
         print("equation to solve=",px)
-    return int(poly.newton_root(px,0))
+    if px.grado==1:
+        return poly.linear_root(px)
+    return px
 
 
 def test() -> bool:
     return main(test_input) == 301
 
+extra_data = """
+root: pppw + drzm
+dbpl: 41
+cczh: sllz + lgvd
+zczc: dvpt - qlgz
+ptdq: humn * humn
+dvpt: lfqf * zstt
+lfqf: 3
+humn: 5
+ljgn: 360
+sjmn: ptdq * dbpl
+sllz: ptdq * ptdq
+pppw: cczh * lfqf
+lgvd: ljgn + wvql
+drzm: hmdt - zczc
+hmdt: 0
+qlgz: bzbn * rjtn
+zstt: rjtn * ptdq
+rjtn: hwpf * humn
+hwpf: 2
+bzbn: 63
+wvql: hmdt - sjmn
+"""
 
+def extra():
+    """https://www.reddit.com/r/adventofcode/comments/zrdfb0/2022_day_21_part_3/"""
+    return math.prod( poly.rational_roots(main(extra_data)) )
 
 if __name__ == "__main__":
     assert test(),"fail test part 2"
     print("pass test part 2")
     data = get_raw_data()
     print("solution part2:", main(data, show=True)) #
+    print("extra problem from \nhttps://www.reddit.com/r/adventofcode/comments/zrdfb0/2022_day_21_part_3/")
+    print("solution readit:", extra())
     
 
 
